68e7047f0ab2f99f0e512a036a12ad0b1ac9370c
Alexandre <alex.silva250@hotmail.com>
2025-10-09 22:42:00 -0300

    fix: import optimization

--- book-net/src/main/java/tech/afsilva/book/auth/AuthenticationService.java
+++ book-net/src/main/java/tech/afsilva/book/auth/AuthenticationService.java
@@ -1,6 +1,5 @@
 package tech.afsilva.book.auth;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
