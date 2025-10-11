53e61f462e908748c5798b3c06f8de50c2d69dac
Alexandre <alex.silva250@hotmail.com>
2025-10-10 23:56:28 -0300

    feat: mail sender implementation

---
+++ book-net/src/main/java/tech/afsilva/book/email/EmailTemplate.java
@@ -0,0 +1,12 @@
+package tech.afsilva.book.email;
+
+public enum EmailTemplate {
+
+    ACTIVATE_ACCOUNT("activate_account");
+
+    private final String name;
+
+    EmailTemplate(String name) {
+        this.name = name;
+    }
+}
